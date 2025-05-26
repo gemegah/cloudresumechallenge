820c3d0e18417c2ecd9e8882ff285476f7742c95
Emmanuel <gemegahprince9@gmail.com>
2025-05-26 20:19:58 +0000

    updates

--- iPortfolio-1.0.0/assets/js/main.js
+++ iPortfolio-1.0.0/assets/js/main.js
@@ -227,3 +227,14 @@
   document.addEventListener('scroll', navmenuScrollspy);
 
 })();
+
+//counter code 
+const counter = document.querySelector(".counter-number");
+
+async function updateCounter() {
+  let response = await fetch("https://h2ihrkvipj5byu32j7chnitubu0vhmnf.lambda-url.us-east-1.on.aws/");
+  let data = await response.json();
+  counter.innerHTML = `Views: ${data}`;
+}
+
+updateCounter();
